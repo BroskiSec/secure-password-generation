f2fccd2de1b8594a521bd7e8fb2d2e32d70497a7
Broski <73410045+Broski-1@users.noreply.github.com>
2022-01-11 02:13:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random, string; amt = input('How many passwords do you want to generate?: ')
 for x in range(int(amt)):
-    # generate a random string using ascii, digits, and punctuation, varying 25-125 in length
+    # generate a random string using ascii, digits, and punctuation, varying 25-125 characters in length
     x = ('').join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=random.randint(25, 125)))
     # we reverse the string as to not be predictable with the random module
     print(x[::-1]) 
