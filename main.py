12b8e8304cfcfc48831d86dc08d29fdf22480000
BroskiSec <broski1800@protonmail.ch>
2022-01-23 06:43:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import random, string; amt = input('How many passwords do you want to generate?: ')
-for x in range(int(amt)):
-    # generate a random string using ascii, digits, and punctuation, varying 25-125 characters in length
-    x = ('').join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=random.randint(25, 125)))
+import random 
+import string
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('amount', help='amount of passwords to generate', type=int)
+args = parser.parse_args()
+
+for x in range(int(args.amount)):
+    # generate a random string using ascii, digits, and punctuation, varying 25-250 characters in length
+    x = ('').join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=random.randint(25, 250)))
     # we reverse the string as to not be predictable with the random module
     print(x[::-1]) 
     print(f'Password length is {len(x)}')
